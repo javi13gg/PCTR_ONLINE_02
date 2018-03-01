1f45f628ec82b885404238cdf557fdd609d4429a
danielsetoubu <36934513+danielsetoubu@users.noreply.github.com>
2018-03-01 09:50:29 +0100

    Número de bolas y campos adicionales
    Se actualiza el número de bolas según la especificación de la práctica y se añaden algunos campos adicionales necesarios para la implementación

--- Billiards.java
+++ Billiards.java
@@ -19,8 +19,10 @@ public class Billiards extends JFrame {
 	private Board board;
 
 	// TODO update with number of group label. See practice statement.
-	private final int N_BALL = 2;
+	private final int N_BALL = 2+3;
 	private Ball[] balls;
+	private Thread[] hilos;
+	private Thread hiloTablero;
 
 	public Billiards() {
 
